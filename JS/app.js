329d61272def2f1c6d40e144f2336ffa50a3fb5f
khaled39ab <khaled39ab@gmail.com>
2022-04-16 17:44:37 +0600

    Meal DB Load data

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,14 @@
 const loadMeal = () =>{
-    const url = `www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+    const inputField = document.getElementById("input-field")
+    const name = inputField.value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+    inputField.value ="";
     fetch(url)
     .then(res => res.json())
-    .then (data => console.log(data))
+    .then (data => displayMeals(data.meals))
 }
+
+document.getElementById("button-search").addEventListener("click", function(){
+    loadMeal()
+});
+
