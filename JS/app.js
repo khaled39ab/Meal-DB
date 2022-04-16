7e9c5c2cfa48a1480ba15bbd23228afae79f0e8a
khaled39ab <khaled39ab@gmail.com>
2022-04-16 17:46:37 +0600

    Meal display perfectly

--- JS/app.js
+++ JS/app.js
@@ -12,3 +12,20 @@ document.getElementById("button-search").addEventListener("click", function(){
     loadMeal()
 });
 
+const displayMeals = meals =>{
+    meals.forEach(meal =>{
+       const searchResult = document.getElementById("search-result")
+       const div = document.createElement("div");
+       div.classList.add("col")
+       div.innerHTML = `
+        <div class="card">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+       `
+       searchResult.appendChild(div)
+    });
+}
