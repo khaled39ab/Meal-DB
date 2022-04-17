6038a457d4b99e31235d186d68a2b159115a0fa1
khaled39ab <khaled39ab@gmail.com>
2022-04-17 23:13:59 +0600

    fixed bug

--- JS/app.js
+++ JS/app.js
@@ -1,35 +1,42 @@
 const errorSec = document.getElementById("error-sec");
+const searchResult = document.getElementById("search-result");
 
 // load meal data 
-const loadMeal = () =>{
+const loadMeal = () => {
     const inputField = document.getElementById("input-field")
-    const name = inputField.value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
-    inputField.value ="";
-    fetch(url)
-    .then(res => res.json())
-    .then (data => displayMeals(data.meals))
-    .catch(err => errorMessage());
-}
+    const searchText = inputField.value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    inputField.value = "";
+
+    if (searchText == '') {
+
+    } 
+    else {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayMeals(data.meals))
+            .catch(err => errorMessage());
+    }
+};
 
 // search button event handler
-document.getElementById("button-search").addEventListener("click", function(){
+document.getElementById("button-search").addEventListener("click", function () {
     loadMeal();
     errorSec.style.display = 'none';
+    searchResult.innerHTML = "";
 });
 
 // error handler
-const errorMessage = () =>{
-    errorSec.style.display = 'block'
-}
+const errorMessage = () => {
+    errorSec.style.display = 'block';
+};
 
 // display meal items 
-const displayMeals = meals =>{
-    meals.forEach(meal =>{
-       const searchResult = document.getElementById("search-result")
-       const div = document.createElement("div");
-       div.classList.add("col")
-       div.innerHTML = `
+const displayMeals = meals => {
+    meals.forEach(meal => {
+        const div = document.createElement("div");
+        div.classList.add("col")
+        div.innerHTML = `
         <div class="card">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
@@ -38,6 +45,7 @@ const displayMeals = meals =>{
             </div>
         </div>
        `
-       searchResult.appendChild(div)
+        searchResult.appendChild(div)
     });
-}
+};
+
